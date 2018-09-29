45919a65f0e2d49243a3f9e8ef6b50de1899b141
cy1997cj <1418296577@qq.com>
2018-09-29 08:48:05 +0800

    Initial Commit

---
+++ test.1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test.1
+//
+//  Created by s20171106522 on 2018/9/29.
+//  Copyright © 2018年 s20171106522. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
